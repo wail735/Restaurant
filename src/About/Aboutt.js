16658fcf3513eac92494d7bc23f3b585e244ce19
wail735 <chennoufwail@gmail.com>
2026-06-30 23:11:20 +0100

    fix: correct .env encoding to UTF-8 to ensure GENERATE_SOURCEMAP=false works

--- src/About/Aboutt.js
+++ src/About/Aboutt.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useTheme } from "@mui/material";
+
 
 export default function Aboutt() {
   const { t } = useTranslation();
